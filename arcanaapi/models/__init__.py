f530974071ea4e9569dcd9e14694571331d8fd8d
Raparker88 <raparker88@gmail.com>
2020-12-09 10:44:30 -0600

    model made for tarotuser

--- arcanaapi/models/__init__.py
+++ arcanaapi/models/__init__.py
@@ -1,1 +1,3 @@
 from .card import Card
+from .sign import Sign
+from .tarotuser import Tarotuser
--- arcanaapi/models/tarotuser.py
+++ arcanaapi/models/tarotuser.py
@@ -5,4 +5,7 @@ from django.contrib.auth.models import User
 class Tarotuser(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.DO_NOTHING,)
-    
+    bio = models.CharField(max_length=500)
+    profile_image = models.ImageField(upload_to="profileimages", height_field=None, width_field=None, max_length=None)
+    astrology = models.ForeignKey("Sign", on_delete=models.CASCADE)
+    card_of_day = models.models.ForeignKey("Card", on_delete=models.CASCADE)
