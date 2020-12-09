5465e7f70c45584533639e65256e0991ecd99a37
Raparker88 <raparker88@gmail.com>
2020-12-09 11:01:01 -0600

    model for reading created

--- arcanaapi/models/__init__.py
+++ arcanaapi/models/__init__.py
@@ -2,3 +2,4 @@ from .card import Card
 from .sign import Sign
 from .tarotuser import Tarotuser
 from .subscription import Subscription
+from .reading import Reading
---
+++ arcanaapi/models/reading.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Reading(models.Model):
+
+    tarotuser = models.ForeignKey("Tarotuser", on_delete=models.CASCADE)
+    date_created = models.DateField(default=timezone.now())
+    name = models.CharField(max_length=50)
+    layout = models.ForeignKey("Layout", on_delete=models.CASCADE)
+    shared = models.BooleanField(default=False)
+    notes = models.CharField(max_length=2000)
