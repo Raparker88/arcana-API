a7e3f5daa5857ad77142ecf153180e6081521451
Raparker88 <raparker88@gmail.com>
2020-12-15 10:25:08 -0600

    subscriptions working

--- arcanaapi/models/subscription.py
+++ arcanaapi/models/subscription.py
@@ -4,8 +4,10 @@ from django.utils import timezone
 
 class Subscription(models.Model):
 
-    user = models.ForeignKey("Tarotuser", on_delete=models.CASCADE, related_name='subscriber')
+    user = models.ForeignKey("Tarotuser", on_delete=models.CASCADE, related_name='subscribers')
     follower = models.ForeignKey("Tarotuser", on_delete=models.CASCADE, related_name='subscriptions')
     date_created = models.DateTimeField(default=timezone.now)
+
+    
 
    
--- arcanaapi/views/user.py
+++ arcanaapi/views/user.py
@@ -6,9 +6,10 @@ from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
 from django.contrib.auth.models import User
-from arcanaapi.models import Tarotuser, Sign
+from arcanaapi.models import Tarotuser, Sign, Subscription, Reading
 from rest_framework.decorators import action
 from django.db.models import Q
+from .reading import ReadingSerializer
 
 
 class Users(ViewSet):
@@ -20,7 +21,7 @@ class Users(ViewSet):
         """
         tarotusers = Tarotuser.objects.all()
 
-        #support query param to search by username 
+        #support query param to search by username or full name
         name = self.request.query_params.get('name', None)
 
         if name is not None:
@@ -31,6 +32,20 @@ class Users(ViewSet):
             for user in users:
                 tarotuser = Tarotuser.objects.get(pk = user.id)
                 tarotusers.append(tarotuser)
+
+        #define value on unmapped property subscribed for each user
+        current_user = Tarotuser.objects.get(user=request.auth.user)
+        for user in tarotusers:
+
+            try:
+                # Determine if the currentuser is subscribed
+                subscription = Subscription.objects.get(
+                    user=user, follower=current_user)
+                user.subscribed = True
+            except Subscription.DoesNotExist:
+                user.subscribed = False
+                
+
 
         serializer = TarotUserSerializer(
             tarotusers, many=True, context={'request': request})
@@ -44,6 +59,17 @@ class Users(ViewSet):
         """
         try:
             tarotuser = Tarotuser.objects.get(pk=pk)
+
+            #define value on unmapped property subscribed for the user
+            current_user = Tarotuser.objects.get(user=request.auth.user)
+
+            try:
+                # Determine if the currentuser is subscribed
+                subscription = Subscription.objects.get(
+                    user=tarotuser, follower=current_user)
+                tarotuser.subscribed = True
+            except Subscription.DoesNotExist:
+                tarotuser.subscribed = False
 
             serializer = TarotUserSerializer(tarotuser, many=False, context={'request': request})
 
@@ -88,6 +114,89 @@ class Users(ViewSet):
 
         return Response(serializer.data)
 
+    @action(methods=['post', 'delete'], detail=True)
+    def subscription(self, request, pk=None):
+        """Managing users subscribing to other user"""
+
+        if request.method == "POST":
+            tarotuser = Tarotuser.objects.get(pk=pk)
+            current_user = Tarotuser.objects.get(user=request.auth.user)
+            
+
+            try:
+                # Determine if the user is already subscribed
+                subscription = Subscription.objects.get(
+                    user=tarotuser, follower=current_user)
+                return Response(
+                    {'message': 'user already follows this user.'},
+                    status=status.HTTP_422_UNPROCESSABLE_ENTITY
+                )
+            except Subscription.DoesNotExist:
+                # The user is not signed up.
+                subscription = Subscription()
+                subscription.user = tarotuser
+                subscription.follower = current_user
+                subscription.save()
+
+                return Response({}, status=status.HTTP_201_CREATED)
+
+        # User wants to unsubscribe
+        elif request.method == "DELETE":
+    
+            try:
+                tarotuser = Tarotuser.objects.get(pk=pk)
+            except Tarotuser.DoesNotExist:
+                return Response(
+                    {'message': 'user does not exist.'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+            # Get the authenticated user
+            current_user = Tarotuser.objects.get(user=request.auth.user)
+
+            try:
+                # Try to delete the subscription
+                subscription = Subscription.objects.get(
+                    follower=current_user, user=tarotuser)
+                subscription.delete()
+                return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+            except Subscription.DoesNotExist:
+                return Response(
+                    {'message': 'Not currently subscribed to user.'},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+
+        # If the client performs a request with a method of
+        # anything other than POST or DELETE, tell client that
+        # the method is not supported
+        return Response({}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    @action(methods=['get'], detail=False)
+    def subscriptions(self, request):
+        """Managing GET requests to return a users subscriptions"""
+
+        if request.method == "GET":
+            
+            current_user = Tarotuser.objects.get(user=request.auth.user)
+            
+            subscriptions = current_user.subscriptions.all()
+            subscribed_readings=[]
+            for subscription in subscriptions:
+                tarotuser = Tarotuser.objects.get(pk = subscription.user_id)
+                readings = Reading.objects.filter(tarotuser = tarotuser)
+                for reading in readings:
+                    subscribed_readings.append(reading)
+                
+
+            serializer = ReadingSerializer(subscribed_readings, many=True, context={'request': request})
+
+            return Response(serializer.data)
+            
+
+        #if the client performs a request other than GET
+        return Response({}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
 class SignSerializer(serializers.ModelSerializer):
     """JSON serializer for astrological sign"""
 
@@ -102,4 +211,4 @@ class TarotUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Tarotuser
-        fields = ("id", "bio",  "full_name", "profile_image", "username", "astrology")
+        fields = ("id", "bio",  "full_name", "profile_image", "username", "astrology", "subscribed")
