34b8603ea794d8e4dcbf9efd409d8785df48119e
Raparker88 <raparker88@gmail.com>
2020-12-16 13:54:17 -0600

    getting register to work

--- arcanaapi/views/__init__.py
+++ arcanaapi/views/__init__.py
@@ -3,4 +3,5 @@ from .auth import register_user
 from .card import Cards
 from .reading import Readings
 from .comment import Comments
-from.user import Users
+from .user import Users
+from .sign import Signs
